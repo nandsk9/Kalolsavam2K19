8f7255f41714a2f89a3a71d03f032a10fa415ecc
Nanda Kishor vc <nandsk9@users.noreply.github.com>
2019-11-20 12:51:38 +0530

    first-comit

---
+++ app/src/main/java/com/wish/kalolsavam2k19/ActivityOne.java
@@ -0,0 +1,21 @@
+package com.wish.kalolsavam2k19;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+        TextView txtInfo = findViewById(R.id.txtInfo);
+        if(getIntent() != null)
+        {
+            String info = getIntent().getStringExtra("info");
+            txtInfo.setText(info);
+        }
+    }
+}
